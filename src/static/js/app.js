4ae1eef6a5221dcaa37a42680a7ce2f053b5461e
Delgermaa1230 <145009550+Delgermaa1230@users.noreply.github.com>
2023-12-17 18:44:58 +0800

    .

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,17 +53,20 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
-            )}
-            {items.map(item => (
-                <ItemDisplay
-                    item={item}
-                    key={item.id}
-                    onItemUpdate={onItemUpdate}
-                    onItemRemoval={onItemRemoval}
-                />
-            ))}
-        </React.Fragment>
+                <p className="text-center">You have no todo items yet! Add one above!</p>
+    )
+}
+{
+    items.map(item => (
+        <ItemDisplay
+            item={item}
+            key={item.id}
+            onItemUpdate={onItemUpdate}
+            onItemRemoval={onItemRemoval}
+        />
+    ))
+}
+        </React.Fragment >
     );
 }
 
@@ -152,9 +155,8 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
                         }
                     >
                         <i
-                            className={`far ${
-                                item.completed ? 'fa-check-square' : 'fa-square'
-                            }`}
+                            className={`far ${item.completed ? 'fa-check-square' : 'fa-square'
+                                }`}
                         />
                     </Button>
                 </Col>
